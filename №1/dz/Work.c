6955e4d702c96176c335bf590f6ba3f234f1e914
Карина Теперик <teperik.karina2007@gmail.com>
2026-09-10 09:30:47 +0300

    Add files via upload

---
+++ №1/dz/Work.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    puts("   /\\");
+    puts("  /  \\");
+    puts(" /    \\");
+    puts("/______\\");
+    puts("|      |");
+    puts("|  []  |");
+    puts("|______|");
+    return 0;
+}
